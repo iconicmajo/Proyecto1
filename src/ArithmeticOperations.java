ad0f5b8eab7150eb8dfc2db379137b668fcf669f
amado-developer <amado_developer@hotmail.com>
2019-03-16 11:43:00 -0600

    almost

--- src/ArithmeticOperations.java
+++ src/ArithmeticOperations.java
@@ -6,39 +6,94 @@ public class ArithmeticOperations {
     private Stack<String> operator = new Stack<>();
     private LinkedList<Integer> operands = new LinkedList<>();
     private LinkedList<Integer> result = new LinkedList<>();
-   // private String tempFunction = "";
-    /*
-    private int bracketsComparatorRight= 0;
-    private int bracketsComparatorLeft = 0;
-    private int numberA = 0, numberB = 0;*/
-
-    int total; //important
+    private String tempFunction = "";
+    private int tempFunctionSize;
+    private String currentOperator;
+    private boolean assignationNotActive = false;
+    private int total;
 
     public ArithmeticOperations(String function){
         int functionSize = function.length();
         int functionCounter = 0;
-        int total = 0;
+        String number = "";
         while (true){
 
+            if(operator.size() > 0 && assignationNotActive == false){
+                if(operator.get(0) == "+" || operator.get(0) == "-"){
+                    total = 0;
+                }else{
+                    total = 1;
+                }
+                assignationNotActive = true;
+            }
             if(Character.isDigit(function.charAt(functionCounter))){
-                operands.addFirst((int) function.charAt(functionCounter));
+                if(Character.isDigit(function.charAt(functionCounter + 1))){
+                    String twoDigits = Character.toString(function.charAt(functionCounter)) + Character.toString(function.charAt(functionCounter+1));
+                    operands.addLast(Integer.parseInt(twoDigits));
+                    functionCounter++;
+                    tempFunctionSize++;
+                }
+                else{
+                    operands.addLast(Integer.parseInt(Character.toString(function.charAt(functionCounter))));
+                    tempFunction+=function.charAt(functionCounter) +" ";
+                    tempFunctionSize++;
+                }
+
+                tempFunctionSize++;
+                functionCounter++;
             }
 
             switch (function.charAt(functionCounter)){
-                case '(': break;
-                case ')': doOperation(""); break;
-                case '+': operator.push("+");break;
-                case '*': operator.push("*");break;
-                case '-': operator.push("-");break;
-                case '/': operator.push("/");break;
+                case '(': tempFunction = ""; tempFunctionSize = 0; break;
+                case ')': doOperation(tempFunction); break;
+                case '+': operator.push("+"); tempFunction+= "+"; break;
+                case '*': operator.push("*"); tempFunction+= "*"; break;
+                case '-': operator.push("-"); tempFunction+= "-"; break;
+                case '/': operator.push("/"); tempFunction+= "/"; break;
             }
-            if(functionCounter == functionSize){
+            if(functionCounter == functionSize - 1){
                 return;
             }
+            functionCounter++;
         }
     }
 
-    private int doOperation(String operation){
-        return  0;
+    private void doOperation(String operation){
+        currentOperator = this.operator.pop();
+
+        int i = 0;
+
+        while (this.tempFunctionSize > 0){
+            if(currentOperator == "+"){
+                if(this.tempFunctionSize > 1){
+                    total += operands.removeFirst() + operands.removeFirst(); tempFunctionSize -=2;
+                }else{
+                    total += operands.removeFirst(); tempFunctionSize--;
+                }
+            }else if(currentOperator == "-"){
+                if(tempFunctionSize > 1){
+                    total -= operands.removeLast() - operands.removeLast(); tempFunctionSize -=2;
+                }else{
+                    total -= operands.removeLast(); tempFunctionSize--;
+                }
+            }else if(currentOperator == "*"){
+                if(tempFunctionSize> 1){
+                    total *= operands.removeLast() * operands.removeLast(); tempFunctionSize -=2;
+                }else{
+                    total *= operands.removeLast(); tempFunctionSize --;
+                }
+            }else if(currentOperator == "/"){
+                if(tempFunctionSize > 1){
+                    total = operands.removeLast() / operands.removeLast() / total; tempFunctionSize -=2;
+                }else{
+                    total /= operands.removeLast(); tempFunctionSize--;
+                }
+            }
+        }
+
+    }
+
+    public int getTotal() {
+        return total;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,12 @@ public class Main {
         Parser parser = new Parser();
         Scanner readLine = new Scanner(System.in);
 
-        String arithmethicOperation = readLine.nextLine();
-        System.out.println(parser.arithmeticOperations(arithmethicOperation));
+        //String arithmethicOperation = readLine.nextLine();
+        //System.out.println(parser.arithmeticOperations(arithmethicOperation));
+        //int a = Integer.parseInt("3 + 3");
+       // System.out.println(3 + 3);
+        ArithmeticOperations a = new ArithmeticOperations("(+ 2 (* 2(+ 12 3)) (* 1 2 3))");
+        System.out.println(a.getTotal());
 
     }
 }
