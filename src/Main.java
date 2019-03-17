3294c1da77d42befea5fe40074df5cc1901748f9
amado-developer <amado_developer@hotmail.com>
2019-03-16 23:16:03 -0600

    some changes to Aritmethic, still does not work fine

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,11 @@ public class Main {
         //System.out.println(parser.arithmeticOperations(arithmethicOperation));
         //int a = Integer.parseInt("3 + 3");
        // System.out.println(3 + 3);
-        //ArithmeticOperations a = new ArithmeticOperations("(+ (* (+ 1 2 3) 2) (+ 4 5 6) 7)");
+        ArithmeticOperations a = new ArithmeticOperations("(+ (* (+ 1 2 3) 2) (+ 4 5 6) 7)");
        //ArithmeticOperations a = new ArithmeticOperations("(+ 2 (+ 3 4 5) (+ 1 2 3) 4)");
         //ArithmeticOperations a = new ArithmeticOperations("(/ 6 2 3)"); CHECK THIS, DOES NOT WORK YET
-        System.out.println(a.getTotal());
+        //System.out.println(a.getTotal());
+
 
     }
 }
